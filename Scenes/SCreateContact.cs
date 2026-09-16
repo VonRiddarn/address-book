c875e12d78505819d679b8a3813b174e10311c71
VonRiddarn <vonriddarn@gmail.com>
2026-09-16 13:04:14 +0200

    feat: add wizard (builder) to showcase

--- Scenes/SCreateContact.cs
+++ Scenes/SCreateContact.cs
@@ -12,13 +12,16 @@ class SCreateContact : IScene
 		Wizard builder = new("== *: Skapa kontakt :* ==");
 
 		string name = builder.GetNext("Namn: ", "Namn: ");
-
 		int age = builder.GetNext<int>("Ålder: ", "Ålder: ", " år");
+		int friends = builder.GetNext<int>("Antal vänner: ", "Du har: ", " vänner!");
+
+
+		builder.RenderValues(includeHeader: true);
 
 		Console.ReadKey();
 
 		Console.Clear();
-		Console.WriteLine($"{name} | {age}");
+		Console.WriteLine($"{name} | {age} -- {friends}");
 
 		Console.ReadKey();
 		return new SceneTransition.Pop();
