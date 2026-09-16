22790ed13246fb05e27b25302f5811508e5c2ec1
VonRiddarn <vonriddarn@gmail.com>
2026-09-16 14:27:02 +0200

    feat: add clear, add and remvoe scaffold

--- src/AddressBook/ContactRepository.cs
+++ src/AddressBook/ContactRepository.cs
@@ -5,56 +5,42 @@ using System.IO;
 static class ContactRepository
 {
 
-	static readonly Dictionary<int, Contact>? _contacts = InitializeContacts();
+	// GUID
+	static readonly Dictionary<string, Contact> _contacts = [];
 
-	static int _currentIndex = 0;
-
-	public static void RemoveContact(Contact contact)
+	public static Contact? RemoveContact(string guid)
 	{
+		bool success = _contacts.Remove(guid, out Contact? contact);
+		SaveContactsToFIle();
 
+		return success ? contact : null;
 	}
 
 	public static void AddContact(Contact contact)
 	{
+		// do
+		// Generate GUID
+		// While(!TryAdd(GUID, contact))
 	}
 
 	public static void Clear()
 	{
-
+		_contacts.Clear();
+		SaveContactsToFIle();
 	}
 
 	static void SaveContactsToFIle()
 	{
-
-	}
-
-	static int GetCurrentIndex()
-	{
-		string fp = "src/data/.INDEXER";
-		string fd = "src/data";
-
-		if (!File.Exists(fp))
-		{
-			Directory.CreateDirectory(fd);
-			File.Create(fp);
-			File.WriteAllText("0", fp);
-			return 0;
-		}
-
-		string sn = File.ReadAllText(fp);
-
-		if (int.TryParse(sn, out int index))
-			return index;
-
-		throw new Exception(".INDEXER has an unparseable value.");
+		// Make JSON from cntact dict
+		// Save JSON to file.
 	}
 
 	static Dictionary<int, Contact> InitializeContacts()
 	{
-		_currentIndex = GetCurrentIndex();
 		// Check if contact file exists...
 		// Initialize using JSON
 		// Send back
+		// Else empty / new
 
 		return [];
 	}
